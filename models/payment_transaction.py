d629587d812c99b1a9a9911be23f0060bd70e712
Adarsh <adarsh808681@gmail.com>
2024-05-17 18:09:59 +0530

    pay u updations

--- models/payment_transaction.py
+++ models/payment_transaction.py
@@ -1,7 +1,10 @@
-from odoo import models
+import urllib
+from odoo import models, _
 from odoo.addons.payment import utils as payment_utils
 import hashlib
 import requests
+
+from odoo.exceptions import ValidationError
 
 
 class PaymentTransaction(models.Model):
@@ -23,19 +26,20 @@ class PaymentTransaction(models.Model):
 
         api_key = self.provider_id.payu_merchant_code
         txn_id = self.reference
-        amount = self.amount
+        amount = int(self.amount)
+        print(type(amount),'ufed')
         product_info = self.reference
         first_name, last_name = payment_utils.split_partner_name(
             self.partner_id.name)
         email = self.partner_email
         salt = self.provider_id.payu_merchant_key
-        surl = "https://4800-111-92-105-22.ngrok-free.app/payment/payu/return"
-        furl = "https://4800-111-92-105-22.ngrok-free.app/payment/payu/return"
+        surl = "https://de34-111-92-105-22.ngrok-free.app/payment/payu/return"
+        furl = "https://de34-111-92-105-22.ngrok-free.app/payment/payu/return"
         hash_string = f"{api_key}|{txn_id}|{amount}|{product_info}|{first_name}|{email}|||||||||||{salt}"
         hash = hashlib.sha512(hash_string.encode()).hexdigest()
         print('hash', hash)
 
-        url = 'https://test.payu.in/_payment'
+        api_url = 'https://test.payu.in/_payment'
         payload = {
             'key': api_key,
             'txnid': txn_id,
@@ -48,14 +52,39 @@ class PaymentTransaction(models.Model):
             'furl': furl,
             'hash': hash,
         }
+        # Encode the parameters for use in the URL
+        encodedParams = urllib.parse.urlencode(payload)
+
+        # Build the URL for the PayU API request
+        # url = api_url + "?" + encodedParams
+        # print('url', url)
+        print('payu values with hash', encodedParams)
         print('payu values with hash', payload)
-        response = requests.post(url, data=payload)
-        print('response text', response.text)
+        headers = {"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.request("POST", api_url, data=encodedParams, headers=headers)
         return payload
 
-    # def _get_tx_from_notification_data(self, provider_code, notification_data):
-    #     print('get tx-find transaction', self)
-    #
-    #
-    # def _process_notification_data(self, notification_data):
-    #     print('process transaction', self)
+    def _get_tx_from_notification_data(self, provider_code, notification_data):
+        """ Override of payment to find the transaction based on Payu data. """
+
+
+        tx_value = super()._get_tx_from_notification_data(provider_code,
+                                                          notification_data)
+        print("thummmm", tx_value)
+        # print(notification_data, "jjjjjjj")
+        # if provider_code != 'payu' or len(tx_value) == 1:
+        #     return tx_value
+        # reference = notification_data.get('txnid')
+        # if not reference:
+        #     raise ValidationError(
+        #         "PayU: " + _("Received data with missing reference (%s)",
+        #                      reference)
+        #     )
+        # tx_value = self.sudo().search([('reference', '=', reference),
+        #                                ('provider_code', '=', 'payu')])
+        # if not tx_value:
+        #     raise ValidationError(
+        #         "PayU: " + _("No transaction found matching reference %s.",
+        #                      reference)
+        #     )
+        # return tx_value
